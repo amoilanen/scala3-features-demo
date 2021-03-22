f3ce3372feb2382d98d16a5dc44910ab6c3d7fa7
Anton Ivanov <anton.al.ivanov@gmail.com>
2021-03-22 11:02:56 +0200

    ADT using Scala 3 enums example

---
+++ src/main/scala/io.github.antivanov/scala3.examples/enums/AlgebraicDataTypes.scala
@@ -0,0 +1,33 @@
+import scala.math._
+
+/*
+ * In Scala 3 it is possible to implement ADTs using enums.
+ */
+enum Tree[+T]:
+  case Empty
+  case Leaf(value: T)
+  case Node(left: Tree[T], right: Tree[T])
+
+import Tree._
+
+def depth[T](tree: Tree[T]): Int =
+  tree match
+    case Empty => 0
+    case Leaf(_) => 1
+    case Node(left, right) => 1 + max(depth(left), depth(right))
+
+@main def alegbraicDataTypes: Unit =
+  val tree = Node(
+    Node(
+      Leaf(1),
+      Empty
+    ),
+    Node(
+      Node(
+        Leaf(2),
+        Leaf(3)
+      ),
+      Leaf(4)
+    )
+  )
+  println(depth(tree)) // 4
