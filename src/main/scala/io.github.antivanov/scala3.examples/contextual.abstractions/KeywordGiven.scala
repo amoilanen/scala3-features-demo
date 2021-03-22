5520a64a356866063ffeec06bfb92e0b468e2fc4
Anton Ivanov <anton.al.ivanov@gmail.com>
2021-03-22 20:43:19 +0200

    Given examples

---
+++ src/main/scala/io.github.antivanov/scala3.examples/contextual.abstractions/KeywordGiven.scala
@@ -0,0 +1,18 @@
+
+trait Show[T]:
+  def show(value: T): String
+
+class Cat(val name: String)
+
+given stringShow: Show[String] with
+  def show(value: String): String =
+    value
+
+given catShow: Show[Cat] with
+  def show(cat: Cat): String =
+    s"cat named ${cat.name}"
+
+@main def KeywordGiven: Unit =
+  val tom = Cat("Tom")
+  val catShow = summon[Show[Cat]]
+  println(catShow.show(tom))
