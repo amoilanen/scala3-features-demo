4b055c9f30800a4abfd544a997ca6bf187311196
Anton Ivanov <anton.al.ivanov@gmail.com>
2021-03-29 10:44:05 +0300

    Importing givens

---
+++ src/main/scala/io.github.antivanov/scala3.examples/contextual.abstractions/ImportingGivens.scala
@@ -0,0 +1,34 @@
+package io.github.antivanov.scala3.examples.contextual_abstractions
+
+trait Show[T]:
+  def show(value: T): String
+
+class Cat(val name: String)
+
+object ShowInstances {
+  given catShow: Show[Cat] with
+    def show(cat: Cat): String =
+      s"cat named ${cat.name}"
+
+  given listShow[T](using s: Show[T]): Show[List[T]] with
+    def show(values: List[T]): String =
+      values.map(s.show(_)).mkString(",")
+
+  extension [T](value: T)(using s: Show[T])
+    def show: String =
+      s.show(value)
+}
+
+@main def importingGivens: Unit =
+  import ShowInstances._
+
+  // Givens should be imported separately in Scala 3
+  import ShowInstances.given Show[Cat]
+  import ShowInstances.given Show[List[?]]
+
+  // Alternatively all of the givens can be imported at once
+  //import ShowInstances.given
+
+  val cats = List(Cat("Tom"), Cat("Garfield"), Cat("Felix"))
+  println(cats.head.show)
+  println(cats.show)
