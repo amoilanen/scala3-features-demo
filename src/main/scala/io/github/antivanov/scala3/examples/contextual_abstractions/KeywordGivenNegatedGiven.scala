297bd24ca68c4089208277594e4781033bb7825d
Anton Ivanov <anton.al.ivanov@gmail.com>
2022-05-09 21:34:31 +0300

    Define separate type for Result

--- src/main/scala/io/github/antivanov/scala3/examples/contextual_abstractions/KeywordGivenNegatedGiven.scala
+++ src/main/scala/io/github/antivanov/scala3/examples/contextual_abstractions/KeywordGivenNegatedGiven.scala
@@ -1,6 +1,6 @@
 package io.github.antivanov.scala3.examples.contextual_abstractions
 
-import io.github.antivanov.scala3.examples.contextual_abstractions.KeywordGivenNegatedGivenMain.toEither
+import io.github.antivanov.scala3.examples.contextual_abstractions.KeywordGivenNegatedGivenMain.toResult
 
 import scala.util.{NotGiven, Try}
 import scala.concurrent.{Await, ExecutionContext, Future}
@@ -10,22 +10,24 @@ object KeywordGivenNegatedGivenMain:
 
   class ToEitherError(cause: Throwable) extends Exception(cause)
 
+  type Result[A] = Either[Throwable, A]
+
   trait KnownMonad[A]
 
   given knownFuture[A]: KnownMonad[Future[A]]()
   given knownEither[A]: KnownMonad[Either[Throwable, A]]()
   given knownOption[A]: KnownMonad[Option[A]]()
 
-  def toEither[A](v: => A)(using NotGiven[KnownMonad[A]]): Either[Throwable, A] =
+  def toResult[A](v: => A)(using NotGiven[KnownMonad[A]]): Result[A] =
     Try(v).toEither.left.map(ToEitherError(_))
 
-  def toEither[A](v: Future[A], timeout: Duration)(using ExecutionContext): Either[Throwable, A] =
-    toEither(Await.result(v, timeout))
+  def toResult[A](v: Future[A], timeout: Duration)(using ExecutionContext): Result[A] =
+    toResult(Await.result(v, timeout))
 
-  def toEither[A, B](v: Either[A, B]): Either[Throwable, B] =
+  def toResult[A, B](v: Either[A, B]): Result[B] =
     v.left.map(_ => ToEitherError(new RuntimeException(s"Encountered $v")))
 
-  def toEither[A](v: Option[A]): Either[Throwable, A] =
+  def toResult[A](v: Option[A]): Result[A] =
     v match {
       case None => Left(ToEitherError(new Exception("Encountered None")))
       case Some(v) => Right(v)
@@ -36,20 +38,20 @@ object KeywordGivenNegatedGivenExtensions:
   import KeywordGivenNegatedGivenMain._
 
   extension[A] (f: Future[A])
-    def asEither(timeout: Duration)(using ExecutionContext): Either[Throwable, A] =
-      toEither(f, timeout)
+    def asResult(timeout: Duration)(using ExecutionContext): Result[A] =
+      toResult(f, timeout)
 
   extension[A] (v: => A)(using NotGiven[KnownMonad[A]])
-    def asEither: Either[Throwable, A] =
-      toEither(v)
+    def asResult: Result[A] =
+      toResult(v)
 
   extension[A, B] (v: Either[A, B])
-    def asEither: Either[Throwable, B] =
-      toEither(v)
+    def asResult: Result[B] =
+      toResult(v)
 
   extension[A] (v: Option[A])
-    def asEither: Either[Throwable, A] =
-      toEither(v)
+    def asResult: Result[A] =
+      toResult(v)
 
 @main def keywordGivenNegatedGivenMain: Unit =
   import KeywordGivenNegatedGivenMain._
@@ -59,26 +61,26 @@ object KeywordGivenNegatedGivenExtensions:
   def throwingFunction(errorMessage: String): String =
     throw new RuntimeException(errorMessage)
 
-  println(toEither("plainValueSuccess"))
-  println(toEither(throwingFunction("plainValueFailure")))
-  println(toEither(Future.successful("futureValueSuccess"), 100.millis))
-  println(toEither(Future {
+  println(toResult("plainValueSuccess"))
+  println(toResult(throwingFunction("plainValueFailure")))
+  println(toResult(Future.successful("futureValueSuccess"), 100.millis))
+  println(toResult(Future {
     throwingFunction("futureValueFailure")
   }, 100.millis))
-  println(toEither(Right("eitherValueSuccess")))
-  println(toEither(Left("eitherValueFailure")))
-  println(toEither(Some("optionValueSuccess")))
-  println(toEither(None))
+  println(toResult(Right("eitherValueSuccess")))
+  println(toResult(Left("eitherValueFailure")))
+  println(toResult(Some("optionValueSuccess")))
+  println(toResult(None))
 
   import KeywordGivenNegatedGivenExtensions._
 
-  println("plainValueSuccess".asEither)
-  println(throwingFunction("plainValueFailure").asEither)
-  println(Future.successful("futureValueSuccess").asEither(100.millis))
+  println("plainValueSuccess".asResult)
+  println(throwingFunction("plainValueFailure").asResult)
+  println(Future.successful("futureValueSuccess").asResult(100.millis))
   println((Future {
     throwingFunction("futureValueFailure")
-  }).asEither(100.millis))
-  println(Right("eitherValueSuccess").asEither)
-  println(Left("eitherValueFailure").asEither)
-  println(Some("optionValueSuccess").asEither)
-  println(None.asEither)
+  }).asResult(100.millis))
+  println(Right("eitherValueSuccess").asResult)
+  println(Left("eitherValueFailure").asResult)
+  println(Some("optionValueSuccess").asResult)
+  println(None.asResult)
