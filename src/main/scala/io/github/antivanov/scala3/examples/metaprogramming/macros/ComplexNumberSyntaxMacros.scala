a54e12ab64a30e686ed6df01fb3fbe04a560408e
Anton Ivanov <anton.al.ivanov@gmail.com>
2022-07-16 19:44:11 +0300

    Add support for complex numbers with the negative imaginary part, add comment, compiler flag to check macros

--- src/main/scala/io/github/antivanov/scala3/examples/metaprogramming/macros/ComplexNumberSyntaxMacros.scala
+++ src/main/scala/io/github/antivanov/scala3/examples/metaprogramming/macros/ComplexNumberSyntaxMacros.scala
@@ -16,6 +16,12 @@ object ComplexNumberSyntaxMacros:
   inline def complexNumber(inline z: Double): ComplexNumber =
     ${ asComplexNumber('z) }
 
+  /*
+   * Just an illustration/idea of the macros, a better implementation would also correctly parse
+   * 1 - i; 2 * i + 3; i * 2 + 3; i; -i; etc.
+   * For this it would be necessary to parse the expression and convert only i to the ComplexNumber, plus define
+   * the methods for operations between the ordinary numbers and complex numbers
+   */
   private def asComplexNumber(z: Expr[Double])(using Quotes): Expr[ComplexNumber] =
     import quotes.reflect.report
     z match
@@ -25,6 +31,12 @@ object ComplexNumberSyntaxMacros:
         toComplexNumber[Float](x, y)
       case '{ ($x: Double) + i * ($y: Double) } =>
         toComplexNumber[Double](x, y)
+      case '{ ($x: Int) - i * ($y: Int) } =>
+        toComplexNumber[Int](x, '{-${y}})
+      case '{ ($x: Float) - i * ($y: Float) } =>
+        toComplexNumber[Float](x, '{-${y}})
+      case '{ ($x: Double) - i * ($y: Double) } =>
+        toComplexNumber[Double](x, '{-${y}})
       case _ =>
         report.error(s"Invalid complex number definition: ${z.show}");
         '{???}
