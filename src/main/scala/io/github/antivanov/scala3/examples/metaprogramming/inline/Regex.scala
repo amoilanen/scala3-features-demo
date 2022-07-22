d68c95d44f6e3a525b8e1e4394669e9826069a68
Anton Ivanov <anton.al.ivanov@gmail.com>
2022-07-23 00:28:41 +0300

    Add comments

--- src/main/scala/io/github/antivanov/scala3/examples/metaprogramming/inline/Regex.scala
+++ src/main/scala/io/github/antivanov/scala3/examples/metaprogramming/inline/Regex.scala
@@ -7,11 +7,21 @@ object Regex
   /*
    * Simplistic regular expressions which support letters, dot and modifiers + and *, for example
    * (:?ab)+c*d.
+   *
+   * We can optimize regex matching by inlining only at the first level of matching state machine since in the Quantifier and
+   * Sequence matching implementation the compiler does not know which implementation of matching to inline for the abstract
+   * Regexs contained within.
+   *
+   * There is a tradeoff - inlining and optimization vs dynamism and being able to compose Regexs together
    */
   trait Regex:
-    def matches(input: String): Boolean =
+    inline def matches(input: String): Boolean =
       matchAt(input, 0) == input.length
 
+    /*
+     * We cannot easily put "inline" on this abstract method: the implementation of `matchAt` in Quantifier and Sequence
+     * will not know which implementation to inline.
+     */
     def matchAt(input: String, position: Int): Int
 
     protected inline final def withinInputLimits(input: String, position: Int): Boolean =
@@ -40,22 +50,17 @@ object Regex
   trait Quantifier extends Regex:
     val regex: Regex
 
-    protected inline final def tryMatchAt(input: String, position: Int): Int =
-      var lastMatch = position
-      var newMatch = regex.matchAt(input, position)
-      while (newMatch != -1 && newMatch != lastMatch)
-        lastMatch = newMatch
-        newMatch = regex.matchAt(input, lastMatch)
-      lastMatch
+    @tailrec
+    protected final def tryMatchAt(input: String, position: Int): Int =
+      val lastMatch = regex.matchAt(input, position)
+      if (lastMatch == -1)
+        position
+      else
+        tryMatchAt(input, lastMatch)
 
     // Cannot use "inline" with tailrec methods, the following will not work:
     //@tailrec
-    //protected inline final def tryMatchAt(input: String, position: Int): Int =
-    //  val lastMatch = regex.matchAt(input, position)
-    //  if (lastMatch == -1)
-    //    position
-    //  else
-    //    tryMatchAt(input, lastMatch)
+    //protected inline final def tryMatchAt(input: String, position: Int): Int
 
   case class PlusQuantifier(regex: Regex) extends Quantifier:
     override inline def matchAt(input: String, position: Int): Int =
